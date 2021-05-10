4b045c8d1e282ed4c2ea051b65494e7bdcf396cc
Rose <rosewin.baker@gmail.com>
2021-05-10 10:44:12 -0500

    Updated web results, added image to README

--- script.js
+++ script.js
@@ -11,15 +11,39 @@ fetch('polishes.json')
             if (data[i].status != "sold_out") {
             // if (data[i].name == "Agave") {
                 console.log(data[i].name)
+
+                // Create anchor div
                 let div = document.createElement('div');
                 div.className = 'test';
+
+                // Create h5 text with product name
                 var h5 = document.createElement("h5");
                 h5.innerHTML = data[i].name;
+
+                // Create image of product
                 var img = document.createElement("img");
                 img.src = data[i].img;
+
+                // Create link to product listing on Ethereal Lacquer website
+                var a = document.createElement('a');
+                a.href = data[i].href;
+                if (data[i].price_sale) {
+                    console.log('Sale item: ' + data[i].name + " selling for " + data[i].price_sale)
+                    a.innerText = 'Purchase Link: ' + data[i].price_sale + ' (Sale Price)';
+                }
+                else {
+                    a.innerText = 'Purchase Link: ' + data[i].price_regular;
+                }
+                
+
+                // Append all the things to the page
                 content.appendChild(div);
                 div.appendChild(h5);
-                div.appendChild(img)
+                div.appendChild(img);
+                div.appendChild(document.createElement('br'));
+                div.appendChild(a)
+
+
             }
             
         }
